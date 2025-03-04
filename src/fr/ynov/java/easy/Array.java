6ce490806855c782a00ac7be83673eac43c4d710
tompass8 <tom.passerman@ynov.com>
2025-03-04 17:34:10 +0100

    (For-While-DoWhile-Array)

---
+++ src/fr/ynov/java/easy/Array.java
@@ -0,0 +1,10 @@
+package fr.ynov.java.easy;
+
+public class Array {
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        for(int i: numbers) {
+            System.out.println(i);
+        }
+    }
+}
---
+++ src/fr/ynov/java/easy/DoWhile.java
@@ -0,0 +1,11 @@
+package fr.ynov.java.easy;
+
+public class DoWhile {
+    public static void main(String[] args) {
+        int i = 0;
+        do {
+            i++;
+            System.out.println(i);
+        } while (i < 10);
+    }
+}
