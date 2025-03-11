b8e199df50ee50649fdda85c57d577387b38b21b
tompass8 <tom.passerman@ynov.com>
2025-03-11 16:56:11 +0100

    (ScanMe.java EvenOrOdd.java Calculator.java)exercice

--- src/fr/ynov/java/easy/EvenOrOdd.java
+++ src/fr/ynov/java/easy/EvenOrOdd.java
@@ -2,7 +2,7 @@ package fr.ynov.java.easy;
 
 public class EvenOrOdd {
     public static void main(String[]args){
-       Scanner scanner = new Scanner (System.in);
+       java.util.Scanner scanner = new java.util.Scanner(System.in);
         System.out.println("entre un nombre pour savoir si il est paire ou impaire");
         int number = scanner.nextInt();
         if (number % 2 == 0){
